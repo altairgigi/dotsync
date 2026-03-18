9a40c1fabfa4aa00e943b8c268376da1eb6e6d47
altairgigi <egidiocampo95@gmail.com>
2026-03-18 11:10:17 +0100

    first commit: init repo

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <dirent.h>
+
+int main(){
+
+    //check if config exist if not create file empty
+    //if file exists start parsing
+
+    //skip commented line
+
+    //if target source exists parse content and save to buffer
+    //otherwise skip it and signal missing file/folder
+
+    //if source destination doesn't exist create folder
+    //create or remake file copying content from buffer
+
+    //procede to next line
+
+    //once parsing complete return success
+
+    return 0;
+}
